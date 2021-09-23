e03e9e30bcba783d359b2de76ad0e51fabdd38fc
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-23 18:06:40 -0500

    Fixed column spacing and hover animation

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const mRightContent = Vue.component('mRightContent', {
         return {
             products: [
                 { id: 1, product_name: 'HP Laptop HP Pavilion 14-dv0007la', product_descrip: '', frs_price: 3950, sec_price: 2999,  principal_dir: '../src/products/laptops/hp/hp_pavilion1.png'},
-                { id: 2, product_name: 'ASUS Zenbook 13 Ultra ', product_descrip: '', frs_price: 3999, sec_price: 4500,  principal_dir: '../src/products/laptops/asus/asus_zenbook1.jpg'}
+                { id: 2, product_name: 'ASUS Zenbook 13 Ultra ', product_descrip: '', frs_price: 3999, sec_price: 4500,  principal_dir: '../src/products/laptops/asus/asus_zenbook1.jpg'},
             ]
         }
     }
