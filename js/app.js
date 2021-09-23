51e9bd4c50eefe93dd9ff240f319e7fbe187be6f
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-23 15:22:52 -0500

    Added Sidebar and content layouts with 1 product

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+const sideNavbar = Vue.component('sideNavbar', {
+    template: '#side-navbar-template',
+    data() {
+        return {}
+    }
+})
+
+const mRightContent = Vue.component('mRightContent', {
+    template: '#mright-content-template',
+    data() {
+        return {
+            products: [
+                { id: 1, product_name: 'HP Laptop HP Pavilion 14-dv0007la', product_descrip: '', frs_price: 3950, sec_price: 2999,  principal_dir: '../src/products/laptops/hp/hp_pavilion1.png'}
+            ]
+        }
+    }
+})
+
+
 const app = new Vue({
     el: '#app',
     data: {}
