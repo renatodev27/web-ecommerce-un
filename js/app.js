dba9b1d0f7569cf0210551133861e6c044ad7948
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-23 20:22:10 -0500

    Added ecom-tec branch with 3 products

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ const mRightContent = Vue.component('mRightContent', {
     data() {
         return {
             products: [
-                { id: 1, product_name: 'HP Laptop HP Pavilion 14-dv0007la', product_descrip: '', frs_price: 3950, sec_price: 2999,  principal_dir: 'src/products/laptops/hp/hp_pavilion1.png'},
-                { id: 2, product_name: 'ASUS Zenbook 13 Ultra ', product_descrip: '', frs_price: 3999, sec_price: 4500,  principal_dir: 'src/products/laptops/asus/asus_zenbook1.jpg'},
+                { id: 1, product_name: 'HP Laptop HP Pavilion 14-dv0007la', product_brand: 'HP', product_descrip: '', frs_price: 320, sec_price: 269.99,  principal_dir: 'src/products/laptops/hp/hp_pavilion1.png'},
+                { id: 2, product_name: 'ASUS Zenbook 13 Ultra ', product_brand: 'ASUS', product_descrip: '', frs_price: 899, sec_price: 785,  principal_dir: 'src/products/laptops/asus/asus_zenbook1.jpg'},
+                { id: 3, product_name: 'ASUS Zenbook 13 Ultra ', product_brad: 'LG', product_descrip: '', frs_price: 480.50, sec_price: '',  principal_dir: 'src/products/monitors/lg/monitor_lg1.jpg'},
             ]
         }
     }
